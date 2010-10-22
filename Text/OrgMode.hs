2540d307face46374f776ed9f71e0bec17a83c82
Brent Yorgey <byorgey@cis.upenn.edu>
2010-10-22 23:03:04 +0400

    add pretty-printing stuff and more documentation
    Ignore-this: b95e4c3d4409e1b5536bf588856c8916
    
    darcs-hash:20101022190304-1e371-55467d3ca47d5c4d33ae7e549f66a6c485d1906c

--- Text/OrgMode.hs
+++ Text/OrgMode.hs
@@ -1,17 +1,40 @@
 {-# LANGUAGE FlexibleContexts #-}
--- Simple parser/pretty-printer for emacs org-mode files.
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Text.OrgMode
+-- Copyright   :  (c) Brent Yorgey 2010
+-- License     :  BSD3-style (see LICENSE)
+--
+-- Maintainer  :  byorgey@gmail.com
+-- Stability   :  experimental
+-- Portability :  portable (uses only FlexibleContexts)
+--
+-- Tools for working with emacs org-mode files, including parsers,
+-- pretty-printers, and an assortment of transformations.
+--
+-- XXX include some examples here
+-----------------------------------------------------------------------------
 
 module Text.OrgMode (
   -- * Common types
   -- $types
 
+  -- ** Org-mode elements
     Tag, Space, OrgEltRaw(..), OrgElt(..), OrgEltC
+
+  -- ** Pretty-printing
+  , Pretty, toString
 
   -- * Line-by-line parsing/printing
   -- $linebyline
 
   , OrgFlat(..)
+
+  -- ** Parsing
   , readOrgFlatFile, readOrgFlat
+
+  -- ** Printing
+
   ) where
 
 import System.FilePath
@@ -25,7 +48,7 @@ import Text.PrettyPrint hiding (char, empty, space, text)
 
 import Data.Monoid
 import Control.Applicative hiding (many, (<|>), optional)
-import Control.Arrow
+import Control.Arrow ((+++))
 import Data.Traversable (sequenceA)
 
 ------------------------------------------------------------
@@ -71,14 +94,53 @@ instance OrgEltC OrgElt where
   text    = Text
   sectionHead n _ title _ tags _ = SectionHead n title tags
 
+-- | Class for pretty-printable things.
+class Pretty a where
+  ppr :: a -> Doc
+
+-- | Convert pretty-printable things to 'String's.
+toString :: Pretty a => a -> String
+toString = render . ppr
+
+-- | 'OrgElt's can be pretty-printed, inserting nice-looking default
+--   amounts of whitespace as necessary.
+instance Pretty OrgElt where
+  ppr (Text s) = PP.text s
+  ppr (SectionHead n title tags) =
+      PP.text (replicate n '*') <+> PP.text title <> tagSpace <> pprTags tags
+    where tagSpace
+            | null tags = PP.empty
+            | otherwise = PP.text (replicate count ' ')
+            where count = max 2 $ 77 - (n + 1 + length title + tagLen)
+          tagLen = 1 + (sum $ map ((+1) . length) tags)
+
+-- | Pretty-print a list of tags, like @:tag1:tag2:tag3:@.
+pprTags :: [Tag] -> Doc
+pprTags [] = PP.empty
+pprTags ts = colon <> hcat (punctuate colon (map PP.text ts)) <> colon
+
+-- | 'OrgEltRaw's can be pretty-printed straightfowardly, since they
+--   retain all formatting information from the original source.
+instance Pretty OrgEltRaw where
+  ppr (TextR s) = PP.text s
+  ppr (SectionHeadR n sp1 title sp2 tags sp3) =
+      PP.text (replicate n '*') <> PP.text sp1
+   <> PP.text title <> PP.text sp2
+   <> pprTags tags <> PP.text sp3
+
 ------------------------------------------------------------
 -- Line-by-line parsing/pretty-printing
 ------------------------------------------------------------
 
 -- $linebyline
--- It is always possible to parse org-mode files line-by-line.  This
--- section defines a \"flat\" org-mode parser which simply produces a
--- list of elements, one per line.
+
+-- It is always possible to parse org-mode files line-by-line:
+-- meaningful syntactic units (e.g. links) may not span multiple
+-- lines.  This section defines a \"flat\" org-mode parser which
+-- simply produces a list of elements, one per line.  This may be
+-- sufficient for some applications that don't care about nested
+-- structure, and is simpler to deal with (using 'map', 'filter', and
+-- so on).
 --
 -- It also defines a printer for this flat org-mode structure. If
 -- 'OrgEltRaw' is used, enough information is kept in the parsed AST
@@ -89,6 +151,9 @@ instance OrgEltC OrgElt where
 --   elements were read.
 data OrgFlat a = OrgFlat FilePath [a]
   deriving (Show)
+
+----------------------------------------
+-- Parsing
 
 -- | Parse the given org-mode file into a flat (line-by-line)
 --   'OrgFlat' structure. The type chosen for the elements determines
@@ -128,17 +193,6 @@ parseSectionHead
                 <*  eof
   <?> "section heading"
 
-tryAhead = try . lookAhead
-
-blank :: Parser Char
-blank = tab <|> char ' '
-
-blanks :: Parser String
-blanks = many blank <?> "blanks"
-
-word :: Parser String
-word = many alphaNum <?> "word"
-
 -- | Parse a sequence of zero or more tags surrounded by colons, like :foo:bar:
 parseTags :: Parser [Tag]
 parseTags = char ':' *> endBy word (char ':') <|> return []
@@ -147,22 +201,12 @@ parseTags = char ':' *> endBy word (char ':') <|> return []
 -- | Treat the entire line as raw text.
 parseTextLine :: OrgEltC a => Parser a
 parseTextLine = text <$> many anyChar
-{-
--- Pretty-printing
 
-formatOrgR :: OrgR -> Doc
-formatOrgR (OrgR _ chunks) = vcat (map formatChunkR chunks) <> PP.char '\n'
+----------------------------------------
+-- Printing
 
-formatChunkR :: ChunkR -> Doc
-formatChunkR (TextR s) = PP.text s
-formatChunkR (SectionR n sp1 title sp2 tags sp3) =
-    PP.text (replicate n '*') <> PP.text sp1 <> PP.text title <> PP.text sp2 <>
-    formatTags tags <> PP.text sp3
+-- instance Pretty a => (OrgFlat a) where
 
-formatTags :: [Tag] -> Doc
-formatTags [] = PP.empty
-formatTags ts = colon <> hcat (punctuate colon (map PP.text ts)) <> colon
--}
 
 -- Converting raw format to more structured format
 
@@ -226,3 +270,16 @@ instance Monoid w => Applicative (Either w) where
   (Left w1) <*> (Left w2) = Left (w1 `mappend` w2)
   (Right _) <*> (Left w)  = Left w
   (Right f) <*> (Right x) = Right (f x)
+
+-- Parsing
+
+tryAhead = try . lookAhead
+
+blank :: Parser Char
+blank = tab <|> char ' '
+
+blanks :: Parser String
+blanks = many blank <?> "blanks"
+
+word :: Parser String
+word = many alphaNum <?> "word"
